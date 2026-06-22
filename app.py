de161908d683ae0393b42325f19189376d0bfd8e
namiqqasanov60-hub <namiq.qasanov60@gmail.com>
2026-06-22 19:51:00 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ HTML_TEMPLATE = """
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Telefon üçün vacib olan bu meta etiketini əlavə etdim -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body { font-family: sans-serif; text-align: center; background: #121212; color: white; margin: 0; padding: 20px; }
         .container { max-width: 95%; margin: 20px auto; background: #1e1e1e; padding: 20px; border-radius: 15px; }
-        h2 { margin-bottom: 5px; }
-        .signature { color: #888; font-size: 14px; margin-bottom: 20px; font-style: italic; }
         input { padding: 15px; width: 90%; border-radius: 8px; border: none; margin-bottom: 15px; box-sizing: border-box; }
         button { padding: 15px 30px; background: #ff0050; color: white; border: none; border-radius: 8px; cursor: pointer; width: 90%; }
         video { max-width: 100%; height: auto; margin-top: 20px; border-radius: 10px; }
@@ -16,7 +15,6 @@ HTML_TEMPLATE = """
 <body>
     <div class="container">
         <h2>TikTok Yükləyici</h2>
-        <div class="signature">by Avara Hasan</div>
         <form method="POST">
             <input type="text" name="url" placeholder="Link-i bura yapışdır..." required>
             <br><button type="submit">Tap və Yüklə</button>
